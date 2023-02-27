b39a5eb8e778ea30aa70d4be3186eb28f5705387
Blake Helton <bthelton@gmail.com>
2023-02-27 11:12:37 -0600

    starting the game project

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+
+import pygame
+
+
+class AlienInvasion:
+    '''Overall class to manage assets and behavior'''
+
+'''Initialize the game, and create game resources'''
+def __init__(self):
+    pygame.init()
+
+    self.screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+'''Start the main loop for the day'''
+def run_game(self):
+        while True:
+            '''Watch for keyboard mouse events'''
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            '''Make the most recently drawn screen visible'''
+            pygame.display.flip()
+
+if __name__ == '__main__':
+     '''Make a game instance, and run the game'''
+     ai = AlienInvasion()
+     ai.run_game()
+
+
