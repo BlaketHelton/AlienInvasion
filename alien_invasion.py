77fd09795ea6b08bd3ffff84fcbbd59fc97bbe65
Blake Helton <bthelton@gmail.com>
2023-02-27 14:10:18 -0600

    limiting the ships range

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,8 +35,8 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
                     #move the ship to the right
+                if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
