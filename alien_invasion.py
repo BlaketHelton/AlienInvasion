959fae83155c4b04f601b0f2d3cba411fdf6c8e1
Blake Helton <bthelton@gmail.com>
2023-02-27 13:44:43 -0600

    continuous ship movement to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,16 +24,24 @@ class AlienInvasion:
 
     '''Start the main loop for the game'''
     def run_game(self):
-            while True:
-                self._check_events()
-                self._update_screen()
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-    def _check_event(self):
+    def _check_events(self):
         '''respond to keypresses and mouse events'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #move the ship to the right
+                    self.ship.moving_right += True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                
     def _update_screen(self):
         '''update images on screen, flip to the new screen'''
         self.screen.fill(self.settings.bg_color)
