58a1d42fa9ce860d1818dd6f7412a497a9fdefd6
Blake Helton <bthelton@gmail.com>
2023-02-27 15:07:16 -0600

    storing bullets in a group

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         '''set background color'''
         self.bg_color = (230, 230, 230)
