2f4eb843c9e1de6d744f7fd3a688b71bc274e638
Blake Helton <bthelton@gmail.com>
2023-03-01 19:15:06 -0600

    creating first alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,43 +5,51 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+#from alien import Alien
+
 
 class AlienInvasion:
     '''Overall class to manage assets and behavior'''
 
     '''Initialize the game, and create game resources'''
+
     def __init__(self):
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        #self.aliens = pygame.sprite.Group()
+
+        #self._create_fleet()
 
         '''set background color'''
         self.bg_color = (230, 230, 230)
-
 
     '''Start the main loop for the game'''
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_bullets()
             self._update_screen()
-
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             print(len(self.bullets))
-            
+
     def _update_bullets(self):
         '''Update position of bullets and get rid of old bullets'''
         #Update bullet positions
         self.bullets.update()
-
-        #Get rid of bullets that have disappeared
+#could have problems here because of double code*******
+        # Get rid of bullets that have disappeared
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
@@ -52,10 +60,10 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                #move the ship to the right
+                # move the ship to the right
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                #move the ship to the left
+                # move the ship to the left
                 self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
@@ -68,35 +76,38 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-                
+
     def _check_keyup_events(self, event):
         '''respond to key releases'''
         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False  
+            self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
         '''create a new bullet and add it to the bullets group'''
-        if len(self.bullets) < self.settings.bullets_allowed:
+        if len(self.bullets) <= self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
-                
+    #def _create_fleet(self):
+        '''Create the fleet of aliens'''
+        # make an alien
+        #alien = Alien(self)
+        #self.aliens.add(alien)
+
     def _update_screen(self):
         '''update images on screen, flip to the new screen'''
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-        
+        #self.aliens.draw(self.screen)
+
         pygame.display.flip()
 
+
 if __name__ == '__main__':
-     '''Make a game instance, and run the game'''
-     ai = AlienInvasion()
-     ai.run_game()
-    
-
-
-
+    '''Make a game instance, and run the game'''
+    ai = AlienInvasion()
+    ai.run_game()
