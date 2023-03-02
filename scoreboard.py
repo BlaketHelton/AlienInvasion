73ba1074b3bab973ef9a74a93a46f034a47d0bce
Blake Helton <bthelton@gmail.com>
2023-03-01 22:57:33 -0600

    displaying the level

--- scoreboard.py
+++ scoreboard.py
@@ -45,6 +45,7 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        
 
     def check_high_score(self):
         '''check to see if theres a new high score'''
@@ -52,6 +53,16 @@ class Scoreboard:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
             self.prep_level()
+
+    def prep_level(self):
+        '''turn the level into a rendered image'''
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        #position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_level(self):
         '''turn the level into a rendered image'''
